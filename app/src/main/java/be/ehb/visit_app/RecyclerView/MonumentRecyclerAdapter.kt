055279110bb760498a2e9ecdc38607db415a0125
Jonathan De Wit <jonathan.de.wit@student.ehb.be>
2022-08-16 08:58:57 +0200

    final commit

--- app/src/main/java/be/ehb/visit_app/RecyclerView/MonumentRecyclerAdapter.kt
+++ app/src/main/java/be/ehb/visit_app/RecyclerView/MonumentRecyclerAdapter.kt
@@ -2,6 +2,7 @@ package be.ehb.visit_app.RecyclerView
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -43,12 +44,12 @@ class MonumentRecyclerAdapter (var monuments: List<Monument>, var favoriteMonume
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonumentViewHolder {
-        var monumentItemView = LayoutInflater.from(parent.context).inflate(R.layout.monument_list_item, parent, false)
+        val monumentItemView = LayoutInflater.from(parent.context).inflate(R.layout.monument_list_item, parent, false)
         return MonumentViewHolder(monumentItemView)
     }
 
     override fun onBindViewHolder(holder: MonumentViewHolder, position: Int) {
-        var monument = monuments.get(position)
+        val monument = monuments.get(position)
         holder.nameTextView.text = monument.name
         holder.kindtextView.text = holder.kindtextView.text.toString().plus(": ${monument.kinds}")
         holder.moreInfoButton.setOnClickListener {
@@ -56,10 +57,15 @@ class MonumentRecyclerAdapter (var monuments: List<Monument>, var favoriteMonume
             intent.putExtra(MainActivity.EXTRA_MONUMENT_API_ID, monument.apiId)
             context?.startActivity(intent)
         }
+
         holder.favoriteButton.setOnClickListener {
             if (!favoritMonuments.any { m -> m.apiId.equals(monument.apiId)}){
                 runBlocking {
+                    val list = mutableListOf<FavoriteMonument>(monument.toFavoriteMonument())
+                    list+=favoritMonuments
+                    favoritMonuments = list
                     withContext(Dispatchers.IO){
+                        holder.favoriteButton.background.setTint(Color.RED)
                         favoriteMonumentDAO.insertMonument(monument.toFavoriteMonument())
                     }
                 }
@@ -67,6 +73,7 @@ class MonumentRecyclerAdapter (var monuments: List<Monument>, var favoriteMonume
                 runBlocking {
                     withContext(Dispatchers.IO){
                         favoriteMonumentDAO.deleteMonument(monument.toFavoriteMonument())
+                        holder.favoriteButton.background.setTint(Color.GRAY)
                     }
                 }
             }
